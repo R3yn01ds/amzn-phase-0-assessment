5ef3e08887881ec064fbf667b9e3c067d0db6bbe
johnsey <johnsey.reynolds@gmail.com>
2022-04-10 23:42:19 -0400

    Done with assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,11 +1,41 @@
 // 1. Write your functions here
 
+const katzDeli = []
 
+const line = []
+
+function currentline(katzDeli) {
+    for (let i = 0; i < katzDeli.length; i++) {
+        line.push(`` + [i + 1] + `.` + katzDeli[i])
+    }
+    if (katzDeli.length === 0) {
+        return "The line is currently empty."
+      } else
+      return(`The line is currently:` + line)
+}
+
+function takeANumber(katzDeli, name) {
+    katzDeli.push(`${name}`)
+    return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+  }
+
+
+function nowServing(katzDeli) {
+    let i = 0;
+    while (i < katzDeli.length) {
+      i++;
+    }
+    if (katzDeli.length === 0) {
+      return "There is nobody waiting to be served!"
+    }
+    else
+    return (`Currently serving ${katzDeli.shift()}.`)
+  }
+  
+  
 // 2. Example Usage
 
-// const katzDeli = []
-
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
+//  takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 // takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
